0c3cc2b77e04d67e3fecc1e6b62637530741ea5d
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-01-09 18:36:44 -0300

    V2.0

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-var thinkergif = ["assets/images/thinker01.gif","assets/images/thinker02.gif"];
-var thinkerimg = ["assets/images/thinker01.png","assets/images/thinker02.png"];
+var thinkergif = ["assets/images/thinker01.gif","assets/images/thinker02.gif", "assets/images/thinker03.gif","assets/images/thinker04.gif","assets/images/thinker05.gif", "assets/images/thinker06.gif"];
+var thinkerimg = ["assets/images/thinker01.png","assets/images/thinker02.png", "assets/images/thinker03.png","assets/images/thinker04.png","assets/images/thinker05.png", "assets/images/thinker06.png"];
 var thinkerclasses = document.getElementsByClassName('escritores-pensadores-container-painel-imagem');
 
 function TrocarImagem(tc, tg){
